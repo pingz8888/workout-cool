d9c35b88fd0a1fdb5f1bc761849cf0fe813cced5
pingz8888 <hills.02-gives@icloud.com>
2026-06-13 04:59:20 +0800

    Update useForgotPassword.tsx

--- src/features/auth/forgot-password/model/useForgotPassword.tsx
+++ src/features/auth/forgot-password/model/useForgotPassword.tsx
@@ -15,7 +15,7 @@ export const useForgotPassword = () => {
   const forgotPassword = async (email: string, setFieldError: (message: string) => void) => {
     setIsLoading(true);
     try {
-      const { error } = await authClient.resetPassword({
+      const { error } = await authClient.requestPasswordReset({
         email,
         redirectTo: `${getServerUrl()}/${paths.resetPassword}`,
       });
